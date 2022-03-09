b8dc9231ac603d70d39b8a0395cf542ac80b8618
Leandro Menezes <leandromenezes1@gmail.com>
2022-03-09 18:07:18 -0300

    alura: C# Parte 2: Introdução a OO

---
+++ FormacaoCSharp/ByteBank/02-ByteBank/Program.cs
@@ -0,0 +1,11 @@
+﻿using _02_ByteBank;
+
+ContaCorrente conta = new ContaCorrente();
+
+conta.titular = "Gabriela";
+
+Console.WriteLine($"Cliente: {conta.titular}");
+Console.WriteLine($"Agência: {conta.agencia}");
+Console.WriteLine($"Saldo: {conta.saldo}");
+
+Console.ReadLine();
---
+++ FormacaoCSharp/ByteBank/05-ByteBank/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using _05_ByteBank;
+
+//Cliente gabriela = new Cliente();
+
+//gabriela.nome = "Gabriela";
+//gabriela.profissao = "Desenvolvedora C#";
+//gabriela.cpf = "043.562.878-20";
+
+ContaCorrente conta = new ContaCorrente();
+// conta.titular = gabriela;
+
+conta.titular = new Cliente();
+conta.titular.nome = "Gabriela Costa";
+conta.titular.cpf = "235.547.963-36";
+conta.titular.profissao = "Desenvolvedora C#";
+
+conta.saldo = 500;
+conta.agencia = 563;
+conta.numero = 654656;
+
+conta.titular.nome = "Gabriela Costa";
+
+// Console.WriteLine(gabriela.nome);
+Console.WriteLine(conta.titular.nome);
+Console.WriteLine(conta.titular.cpf);
+Console.WriteLine(conta.titular.profissao);
