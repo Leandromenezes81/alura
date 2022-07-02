b2f60f3c6ed56a03dd52d27377b58e5d93f4eda7
Leandro Menezes <leandromenezes1@gmail.com>
2022-07-01 22:30:30 -0300

    feat: Formação C# OO - Update

--- FormacaoCSharp/AprendendoCSharp/01-PrimeiroProjeto/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Olá, mundo! Primeiro projeto no Visual Studio.");
-
-Console.WriteLine("Aplicação finalizada...");
-
-Console.ReadLine();
