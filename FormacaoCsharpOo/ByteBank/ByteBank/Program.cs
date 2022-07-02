233409a83789daf41268fbe32da36e3094c09be3
Leandro Menezes <leandromenezes1@gmail.com>
2022-07-01 23:53:40 -0300

    feat: Formação C# OO - init ByteBank project

--- FormacaoCsharpOo/ByteBank/ByteBank/Program.cs
+++ FormacaoCsharpOo/ByteBank/ByteBank/Program.cs
@@ -12,11 +12,11 @@ conta1.numero_agencia = 23;
 conta1.nome_agencia = "Agência Central";
 conta1.saldo = 93.50;
 
-Console.WriteLine($"Titular: {conta1.titular}");
-Console.WriteLine($"Conta: {conta1.conta}");
-Console.WriteLine($"Número da Agência: {conta1.numero_agencia}");
-Console.WriteLine($"Nome da Agência: {conta1.nome_agencia}");
-Console.WriteLine($"Saldo: {conta1.saldo}");
+//Console.WriteLine($"Titular: {conta1.titular}");
+//Console.WriteLine($"Conta: {conta1.conta}");
+//Console.WriteLine($"Número da Agência: {conta1.numero_agencia}");
+//Console.WriteLine($"Nome da Agência: {conta1.nome_agencia}");
+//Console.WriteLine($"Saldo: {String.Format("{0:0.00}", conta1.saldo)}");
 
 ContaCorrente conta2 = new ContaCorrente();
 
@@ -26,10 +26,26 @@ conta2.numero_agencia = 23;
 conta2.nome_agencia = "Agência Central";
 conta2.saldo = 10000093.50;
 
-Console.WriteLine($"Titular: {conta2.titular}");
-Console.WriteLine($"Conta: {conta2.conta}");
-Console.WriteLine($"Número da Agência: {conta2.numero_agencia}");
-Console.WriteLine($"Nome da Agência: {conta2.nome_agencia}");
-Console.WriteLine($"Saldo: {conta2.saldo}");
+//Console.WriteLine($"Titular: {conta2.titular}");
+//Console.WriteLine($"Conta: {conta2.conta}");
+//Console.WriteLine($"Número da Agência: {conta2.numero_agencia}");
+//Console.WriteLine($"Nome da Agência: {conta2.nome_agencia}");
+//Console.WriteLine($"Saldo: {String.Format("{0:0.00}", conta2.saldo)}");
+
+ContaCorrente conta3 = new ContaCorrente();
+
+//Console.WriteLine($"Titular: {conta3.titular}");
+//Console.WriteLine($"Conta: {conta3.conta}");
+//Console.WriteLine($"Número da Agência: {conta3.numero_agencia}");
+//Console.WriteLine($"Nome da Agência: {conta3.nome_agencia}");
+//Console.WriteLine($"Saldo: {String.Format("{0:0.00}", conta3.saldo)}");
+
+double saldo1 = 100;
+double saldo2 = 100;
+
+saldo2 = conta1.saldo;
+conta1.saldo = 200;
+Console.WriteLine(saldo1 == conta1.saldo);
+Console.WriteLine(saldo1 == saldo2);
 
 Console.ReadKey();
