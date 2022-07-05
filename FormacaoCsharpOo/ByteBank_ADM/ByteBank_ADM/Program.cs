f16ffbe3f7c00ea0bcdabf9388d041dcdbfe378d
Leandro Menezes <leandromenezes1@gmail.com>
2022-07-04 22:52:54 -0300

    feat: Formação C# OO - ByteBank_ADM project init

---
+++ FormacaoCsharpOo/ByteBank_ADM/ByteBank_ADM/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ByteBank_ADM.Funcionarios;
+
+Console.WriteLine("Boas vindas ao ByteBank ADM.");
+
+Funcionario pedro = new Funcionario();
+pedro.Nome = "Pedro";
+pedro.Cpf = "123456789-00";
+pedro.Salario = 2000;
+
+Console.WriteLine($"Bonificação: R$ {pedro.GetBonificacao()}");
+
